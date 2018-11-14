df8fdeb01ae074d999d6c2eddde6f9df3d987de3
Anja Istenic <aistenic1@gmail.com>
2018-11-14 13:09:00 +0100

    created Review model and added validations to Review and Restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,9 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true
+  validates :category, inclusion: {
+    in: %w(italian chinese japanese french belgian)
+  }
 end
